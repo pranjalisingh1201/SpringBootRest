8c5b76fa15ca09e1568e838d9134df2166105f38
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-08-01 10:40:16 +0530

    AOP logging

--- src/main/java/com/springboot/rest/AspectLogging.java
+++ src/main/java/com/springboot/rest/AspectLogging.java
@@ -1,5 +1,8 @@
 package com.springboot.rest;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -11,9 +14,26 @@ import org.springframework.stereotype.Component;
 public class AspectLogging {
 	public static final Logger LOGGER = LoggerFactory.getLogger(AspectLogging.class);
 	@Before("execution(public * com.springboot.rest.AlienController.getAliens())")
-	public void log ()
+	public void logbefore()
 	{
-		LOGGER.info("GetAliens is Called.");
+		LOGGER.info("Before GetAliens is Called.");
+	}
+	@After("execution(public * com.springboot.rest.AlienController.getAliens())")//Called irrespective of exception -- after finally.
+	public void logafter()
+	{
+		LOGGER.info("After GetAliens is Called.");
+	}
+	
+	@AfterReturning("execution(public * com.springboot.rest.AlienController.getAliens())")//Called after function call is success.
+	public void logafterSuccess()
+	{
+		LOGGER.info("After GetAliens is Called successfully.");
+	}
+	
+	@AfterThrowing("execution(public * com.springboot.rest.AlienController.getAliens())")//Called after function call throwed exception.
+	public void logafterException()
+	{
+		LOGGER.info("After GetAliens throwed issue.");
 	}
 
 }
