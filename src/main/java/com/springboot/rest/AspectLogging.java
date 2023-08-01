fa79b415809fa328a464c762e306167464ffd1cf
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-08-01 10:05:06 +0530

    AOP logging

--- src/main/java/com/springboot/rest/AspectLogging.java
+++ src/main/java/com/springboot/rest/AspectLogging.java
@@ -2,16 +2,18 @@ package com.springboot.rest;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class AspectLogging {
-	
+	public static final Logger LOGGER = LoggerFactory.getLogger(AspectLogging.class);
 	@Before("execution(public * com.springboot.rest.AlienController.getAliens())")
 	public void log ()
 	{
-		System.out.println("GetAliens is Called.");
+		LOGGER.info("GetAliens is Called.");
 	}
 
 }
